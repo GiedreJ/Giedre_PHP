483d755097899e9454d6e620b8d1c34c0a3a0a3c
GiedreJ <saulevakarine1@gmail.com>
2019-04-16 16:17:06 +0300

    update05

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 <html>
     <head>
-        <title><?php print "Aš," . date('D') . "ir PHP"; ?>!</title>
+        <title><?php print "Aš, " . date('D') . " ir PHP"; ?>!</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
     <body>
         <h1>
-            Giedrė-HTML <?php print "ir PHP"; ?> asė jau nuo <?php print date("Y") . "metų"; ?>
+            Giedrė-HTML <?php print " ir PHP "; ?> asė jau nuo <?php print date("Y") . " metų "; ?>
         </h1>
         <p>
-            Viskas prasidėjo <?php print date("M") . "mėnesio," . date("D") . "dieną!"; ?>
+            Viskas prasidėjo <?php print date("M") . " mėnesio, " . date("D") . " dieną! "; ?>
         </p>
     </body>
 </html>
