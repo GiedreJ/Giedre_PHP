ba2571af9fb870aba8fcaf9fad658decb0c58059
GiedreJ <saulevakarine1@gmail.com>
2019-04-16 15:38:54 +0300

    update03

--- index.php
+++ index.php
@@ -2,16 +2,16 @@
 
 <html>
     <head>
-        <title>Aš valdau HTML ir PHP!</title>
+        <title>Aš valdau HTML <?php print "ir PHP" ?>!</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
     <body>
         <h1>
-            Giedrė-HTML <?php print ir PHP ?> asė!
+            Giedrė-HTML <?php print "ir PHP" ?> asė!
         </h1>
         <p>
-            Kuriu dinaminį turinį. Atidaryta: <?php print date("$GLOBALSformat", $GLOBALS) ?>
+            Kuriu dinaminį turinį. Atidaryta: <?php print date("Y-m-d H:i:s") ?>
         </p>
     </body>
 </html>
