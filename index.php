e0b221905dc53ba22c28e26f3493284c891433f6
GiedreJ <saulevakarine1@gmail.com>
2019-04-16 15:34:06 +0300

    update02

--- index.php
+++ index.php
@@ -1,1 +1,18 @@
 <?php print "veikia su Git!" ?>
+
+<html>
+    <head>
+        <title>Aš valdau HTML ir PHP!</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <h1>
+            Giedrė-HTML <?php print ir PHP ?> asė!
+        </h1>
+        <p>
+            Kuriu dinaminį turinį. Atidaryta: <?php print date("$GLOBALSformat", $GLOBALS) ?>
+        </p>
+    </body>
+</html>
+
