0b9038bb9fbbd7619b130ecc05b7d066e3d1d86f
GiedreJ <saulevakarine1@gmail.com>
2019-04-16 16:58:31 +0300

    update06

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 <html>
     <head>
-        <title><?php print "Aš, " . date('D') . " ir PHP"; ?>!</title>
+        <title> PHP lydės ir <?php print date('D', (strtotime ('-1 day'))); ?>!</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
     <body>
         <h1>
-            Giedrė-HTML <?php print " ir PHP "; ?> asė jau nuo <?php print date("Y") . " metų "; ?>
+            Giedrė - PHP su manim buvo ir <?php print date('H', (strtotime ('-1 hour'))) . ' valandą '; ?>
         </h1>
         <p>
-            Viskas prasidėjo <?php print date("M") . " mėnesio, " . date("D") . " dieną! "; ?>
+            <?php print date ('Y', (strtotime ('-1 year'))); ?>
         </p>
     </body>
 </html>
