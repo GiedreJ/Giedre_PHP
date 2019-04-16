47dc12c84b0d4257e1dc7638c2a9f7602fa98ee7
GiedreJ <saulevakarine1@gmail.com>
2019-04-16 17:02:32 +0300

    update08

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <html>
     <head>
-        <title> PHP lydės ir <?php print date('D', (strtotime ('-1 day'))); ?>!</title>
+        <title> PHP lydės ir <?php print date('D', (strtotime ('+1 day'))); ?>!</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
